c7b7d7206e57979325af0a614ac56832c7cb384a
hellomatia <shuaigejp80@gmail.com>
2024-05-18 00:07:49 +0900

    [ArrayList]: JavaDiverCustomArrayList 어노테이션 추가

---
+++ src/main/java/List/ArrayList/JavaDiverCustomArrayList.java
@@ -0,0 +1,8 @@
+package List.ArrayList;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JavaDiverCustomArrayList {
+}
